97a1c308565f0cd77850db5abbbb53fba63d0e74
paulokatho <pkatho@gmail.com>
2025-03-07 19:27:53 +0000

    feat: Novas implementacoes

--- src/main/java/sequencia/paulofernandes/controller/LabseqController.java
+++ src/main/java/sequencia/paulofernandes/controller/LabseqController.java
@@ -8,8 +8,6 @@ import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import sequencia.paulofernandes.service.LabseqService;
 
-import java.util.List;
-
 @Path("/labseq")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,31 +17,9 @@ public class LabseqController {
     private LabseqService labseqService;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/all")
-    @Operation(
-            summary = "Calcula e retorna o total da sequência l(n) de 0 até 100.000",
-            description = "Este endpoint calcula a sequência de l(n) de 0 até 100.000."
-    )
-    @APIResponse(
-            responseCode = "200",
-            description = "Cálculo com o total da sequência realizado com sucesso",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON)
-    )
-    @APIResponse(
-            responseCode = "500",
-            description = "Erro interno no servidor",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON)
-    )
-    public List<Long> getLabseqValueTotal() {
-
-        // Retorna todos os valores de l(0) a l(100000)
-        return labseqService.calculateLabseqTotal(100000);
-    }
-
-    @GET
     @Path("/{n}")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     @Operation(
             summary = "Calcula e retorna o valor da sequência l(n)",
             description = "Este endpoint calcula o valor da sequência de l(n)"
@@ -51,15 +27,27 @@ public class LabseqController {
     @APIResponse(
             responseCode = "200",
             description = "Cálculo do valor da sequência realizado com sucesso",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+            content = @Content(mediaType = MediaType.TEXT_PLAIN)
     )
     @APIResponse(
             responseCode = "500",
             description = "Erro interno no servidor",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+            content = @Content(mediaType = MediaType.TEXT_PLAIN)
     )
     public String getLabseqValue(@PathParam("n") int n) {
-        long result = labseqService.calculateLabseq(n);
+        if (n <= 0) {
+            throw new NullPointerException("Somente números positivos!");
+        }
+
+        long result = 0;
+        try {
+            result = labseqService.calculateLabseq(n);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return String.valueOf(result);
     }
+
 }
+
--- src/main/java/sequencia/paulofernandes/service/LabseqService.java
+++ src/main/java/sequencia/paulofernandes/service/LabseqService.java
@@ -1,6 +1,7 @@
 package sequencia.paulofernandes.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.WebApplicationException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,36 +12,6 @@ import java.util.Map;
 public class LabseqService {
 
     private Map<Integer, Long> cache = new HashMap<>();
-
-    public List<Long> calculateLabseqTotal(int n) {
-        long startTime = System.currentTimeMillis(); // Inicia a medição do tempo
-
-        // Lista para armazenar os valores da sequência
-        List<Long> sequence = new ArrayList<>(n + 1);
-
-        // Casos base
-        sequence.add(0L); // l(0) = 0
-        sequence.add(1L); // l(1) = 1
-        sequence.add(0L); // l(2) = 0
-        sequence.add(1L); // l(3) = 1
-
-        // Calcula os valores iterativamente
-        for (int i = 4; i <= n; i++) {
-            long value = sequence.get(i - 4) + sequence.get(i - 3);
-            sequence.add(value);
-        }
-
-        long endTime = System.currentTimeMillis(); // Finaliza a medição do tempo
-        long executionTime = endTime - startTime; // Tempo de execução em milissegundos
-
-        // Verifica se o tempo de execução excede 8 segundos
-        if (executionTime > 8000) { // 8000 ms = 8 segundos
-            throw new RuntimeException("O cálculo excedeu o tempo limite de 8 segundos. Tempo gasto: " + executionTime + " ms");
-        }
-
-        System.out.println("Tempo de execução: " + executionTime + " ms");
-        return sequence;
-    }
 
     public long calculateLabseq(int n) {
         long startTime = System.currentTimeMillis();// Inicia a medição do tempo
@@ -75,7 +46,6 @@ public class LabseqService {
         if (executionTime > 8000) { // 8000 ms = 8 segundos
             throw new RuntimeException("O cálculo excedeu o tempo limite de 8 segundos. Tempo gasto: " + executionTime + " ms");
         }
-
         System.out.println("Tempo de execução: " + executionTime + " ms");
 
         return cache.get(n);
